429d75589acd615f0884174700bf8e74fa779f96
ok-josue <87337858+ok-josue@users.noreply.github.com>
2022-05-31 16:56:08 -1000

    still working on appbar...

--- client/src/App.js
+++ client/src/App.js
@@ -11,15 +11,17 @@ function App() {
 	return (
         <div>
             <Navbar />
-			<Link to="/">Home</Link>
-			<Link to="/login">Login</Link>
-			<Link to="/logout">Logout</Link>
-			<Link to="/about">About</Link>
-			<Link to="/users">Users</Link>
+            <Link to="/">Home</Link>
+            <Link to="/login">Login</Link>
+            <Link to="/logout">Logout</Link>
+            <Link to="/about">About</Link>
+            <Link to="/users">Users</Link>
+            <Link to="/navbar">navbar</Link>
 			{/* inside of route declare every route we want in front end. what should render when url matches route */}
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/login" element={<Login />} />
+				<Route path="/navbar" element={<Navbar />} />
 				<Route path="/logout" element={<Logout />} />
 				<Route path="/about" element={<About />} />
 				<Route path="/users" element={<UsersContainer />} />
--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,14 +1,20 @@
-import { Appbar, Toolbar, Box, Typography, Button } from '@mui/material';
+import { AppBar, Toolbar, Box, Typography, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
 	return (
-		<Box sx={{flexGrow: 1}}>
-			<Appbar position='static'>
-				<Typography variant='h5' component='div'>
-					<Link to='/'>Login</Link>
-				</Typography>
-			</Appbar>
+		<Box sx={{ flexGrow: 1 }}>
+			<AppBar position="static">
+				<Toolbar>
+					<Typography variant="h5" component="div">
+						<Link to="/" style={{ textDecoration: 'none', color: 'white' }}>Home</Link>
+					</Typography>
+					<Box alignItems="right," sx={{ flexGrow: 1, textalign: 'right' }}>
+						<Link to="/login" style={{ textDecoration: 'none', color: 'white', marginRight: '15px' }}>Login</Link>
+						<Link to="/signup" style={{ textDecoration: 'none', color: 'white' }}>Sign Up</Link>
+					</Box>
+				</Toolbar>
+			</AppBar>
 		</Box>
 	);
 }
