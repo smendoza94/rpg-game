d8e4262f28f113f9457436a3a307251ba9d3e7ec
ok-josue <87337858+ok-josue@users.noreply.github.com>
2022-06-01 14:40:34 -1000

    changed navbar color

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -4,14 +4,46 @@ import { Link } from 'react-router-dom';
 function Navbar() {
 	return (
 		<Box sx={{ flexGrow: 1 }}>
-			<AppBar position="static">
+			<AppBar position="static" style={{ backgroundColor: '#006633' }}>
 				<Toolbar>
 					<Typography variant="h5" component="div">
-						<Link to="/" style={{ textDecoration: 'none', color: 'white' }}>Home</Link>
+						<Link
+							to="/"
+							style={{ textDecoration: 'none', color: 'white' }}
+						>
+							RPG-Game
+						</Link>
 					</Typography>
-					<Box alignItems = "right," sx = {{ flexGrow: 1, textAlign: 'right' }}>
-						<Link to="/login" style={{ textDecoration: 'none', color: 'white', marginRight: '15px' }}>Login</Link>
-						<Link to="/signup" style={{ textDecoration: 'none', color: 'white' }}>Sign Up</Link>
+					<Box
+						alignItems="right,"
+						sx={{ flexGrow: 1, textAlign: 'right' }}
+					>
+						<Link
+							to="/login"
+							style={{
+								textDecoration: 'none',
+								color: 'white',
+								marginRight: '15px',
+							}}
+						>
+							Login
+						</Link>
+						<Link
+							to="/"
+							style={{
+								textDecoration: 'none',
+								color: 'white',
+								marginRight: '15px',
+							}}
+						>
+							Logout
+						</Link>
+						<Link
+							to="/signup"
+							style={{ textDecoration: 'none', color: 'white' }}
+						>
+							Sign Up
+						</Link>
 					</Box>
 				</Toolbar>
 			</AppBar>
@@ -20,3 +52,4 @@ function Navbar() {
 }
 
 export default Navbar;
+
